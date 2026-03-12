02426423fd11fe0cac4f01952e12468889da7232
Andrii <dneprovskaya401@gmail.com>
2026-03-12 18:55:03 +0200

    added qdrant integration

--- script/qdrant_integr.py
+++ script/qdrant_integr.py
@@ -1,5 +1,6 @@
 from qdrant_client import QdrantClient 
 from qdrant_client.models import Distance, VectorParams, PointStruct
+from hash_alg import generate_hash_filepath
 
 client = QdrantClient(host="localhost", port=6333)
 
@@ -11,3 +12,30 @@ client.create_collection(
     )
 )
 
+def add_document(client, collection_name, vector, formated_path, full_path, content):
+
+    id_number = generate_hash_filepath(formated_path)
+
+    point = PointStruct(
+        id=id_number,
+        vector=vector,
+        payload={
+            "formated_path": formated_path,
+            "path": full_path,
+            "content": content
+        }
+    )
+
+    client.upsert(
+        collection_name=collection_name,
+        points=[point]
+    )
+
+def search_similar(client, collection_name, query_vector, limit=3):
+    results = client.search(
+        collection_name=collection_name,
+        query_vector=query_vector,
+        limit=limit
+    )
+
+    return results
