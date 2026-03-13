a0975783b2728f2aa5d3d80178e6e95a922dd74b
Andrii <dneprovskaya401@gmail.com>
2026-03-13 23:33:05 +0200

    added redis listener

---
+++ redis/redis_listener.py
@@ -0,0 +1,10 @@
+import redis
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+pubsub = r.pubsub()
+pubsub.subscribe('__keyevent@__:lpop')
+
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        r.save()
