5841b8b96b543bc47220c236ca671fc991665dd0
Andrii <dneprovskaya401@gmail.com>
2026-03-16 22:24:24 +0200

    added redis test for has panding task

--- tests/test_redis_queue_store.py
+++ tests/test_redis_queue_store.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from redis_implement.redis_queue_store import get_pending_tasks 
+from redis_implement.redis_queue_store import get_pending_tasks, has_pending_tasks
 
 def test_get_pending_tasks_with_files():
     with patch("redis_implement.redis_queue_store.load_queue_state") as mock_load:
@@ -32,3 +32,21 @@ def test_get_pending_task_empty():
 
     assert result == []
 
+def test_has_pending_task_true():
+    with patch("redis_implement.redis_queue_store.get_pending_tasks") as mock_get:
+        mock_get.return_value = [
+            {"path": "note1.md", "action": "create"},
+            {"path": "note2.md", "action": "update"}
+        ]
+
+        result = has_pending_tasks()
+
+        assert result is True
+
+def test_has_panding_tasks_false():
+    with patch("redis_implement.redis_queue_store.get_pending_tasks") as mock_get:
+        mock_get.return_value = []
+
+        result = has_pending_tasks()
+
+        assert result is False
